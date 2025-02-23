e4b04818e90b1814a3a5460636fd2baec3f715a8
chrisvdweth <christian.vdweth@gmail.com>
2025-02-23 13:59:26 +0800

    Bug fixes

--- notebooks/src/utils/libimports/vsm.py
+++ notebooks/src/utils/libimports/vsm.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
-import torch
+from collections import defaultdict
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.cluster import AgglomerativeClustering
