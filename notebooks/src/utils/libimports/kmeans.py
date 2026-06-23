0c80c30ca9455ad1d4b9ece87d5d672f35737123
chrisvdweth <christian.vdweth@gmail.com>
2026-06-23 16:28:43 +0800

    First commit

---
+++ notebooks/src/utils/libimports/kmeans.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+from sklearn.cluster import KMeans
+from sklearn import datasets
+from sklearn.metrics.pairwise import euclidean_distances
