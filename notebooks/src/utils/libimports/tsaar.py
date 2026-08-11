74d8262d4c1ceee736b9c0bdbe6858e01e7782a2
chrisvdweth <christian.vdweth@gmail.com>
2026-08-11 15:42:17 +0800

    First commit

---
+++ notebooks/src/utils/libimports/tsaar.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+from statsmodels.tsa.ar_model import AutoReg
+from statsmodels.tsa.stattools import adfuller, kpss
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import root_mean_squared_error
